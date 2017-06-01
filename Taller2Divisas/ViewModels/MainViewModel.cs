599329ad47281f66ff1eb1c08649e6b5746daec7
alexis0592 <yefry.calderon@udea.edu.co>
2017-06-01 16:37:21 -0500

    * BindablePicker.cs: Mapping web service in observable object,   adding changes in main view
    * MainPage.xaml:
    * Taller2Divisas.csproj:
    * MainViewModel.cs:

--- Taller2Divisas/ViewModels/MainViewModel.cs
+++ Taller2Divisas/ViewModels/MainViewModel.cs
@@ -1,20 +1,184 @@
 ﻿using System;
-
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Reflection;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
+using Taller2Divisas.Models;
 using Xamarin.Forms;
 
 namespace Taller2Divisas.ViewModels
 {
-    public class MainViewModel : ContentPage
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Attributes
+        private bool isRunning;
+        private bool isEnabled;
+        private ExchangeRates exchangeRates;
+        private string message;
+        #endregion
+
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Rate> Rates
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get
+            {
+                return isRunning;
+            }
+            set
+            {
+                if (isRunning != value)
+                {
+                    isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRunning"));
+                }
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return isEnabled;
+            }
+            set
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+                }
+            }
+        }
+
+        public decimal Amount
+        {
+            get;
+            set;
+        }
+
+        public double SourceRate
+        {
+            get;
+            set;
+        }
+
+        public double TargetRate
+        {
+            get;
+            set;
+        }
+
+        public string Message
+        {
+            get{
+                return message;
+            }
+            set{
+                if(message != value){
+                    message = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Message"));
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructors
         public MainViewModel()
         {
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Hello ContentPage" }
-                }
-            };
+            Rates = new ObservableCollection<Rate>();
+
+            IsEnabled = false;
+            GetRates();
         }
+
+
+        #endregion
+
+
+        #region Methods
+
+        private void LoadRates()
+        {
+            Rates.Clear();
+            var type = typeof(Rates);
+            var properties = type.GetRuntimeFields();
+
+            foreach (var property in properties)
+            {
+                var code = property.Name.Substring(1, 3);
+                Rates.Add(new Rate
+                {
+                    Code = code,
+                    TaxRate = (double)property.GetValue(exchangeRates.Rates),
+                });
+            }
+        }
+
+
+        private async void GetRates()
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://openexchangerates.org");
+                var url = "/api/latest.json?app_id=f490efbcd52d48ee98fd62cf33c47b9e";
+                var response = await client.GetAsync(url);
+                if (!response.IsSuccessStatusCode)
+                {
+                    await App.Current.MainPage.DisplayAlert("Error", response.StatusCode.ToString(), "Aceptar");
+                    IsRunning = false;
+                    IsEnabled = false;
+                    return;
+                }
+
+                var result = await response.Content.ReadAsStringAsync();
+                exchangeRates = JsonConvert.DeserializeObject<ExchangeRates>(result);
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", ex.Message, "Aceptar");
+                IsRunning = false;
+                IsEnabled = false;
+                return;
+            }
+
+            LoadRates();
+            IsRunning = false;
+            IsEnabled = true;
+
+        }
+        #endregion
+
+        #region Commands
+        public ICommand ConvertMoneyCommand
+        {
+            get{
+                return new RelayCommand(ConvertMoney);
+            }
+        }
+
+        private void ConvertMoney()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
     }
 }
 
