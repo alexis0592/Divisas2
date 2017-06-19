ad89eec077ea8baebe4065d942ee209310ad8125
alexis0592 <yefry.calderon@udea.edu.co>
2017-06-19 13:53:09 -0500

    adding exchange names

--- Taller2Divisas/ViewModels/MainViewModel.cs
+++ Taller2Divisas/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Net.Http;
@@ -7,6 +8,7 @@ using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
 using Newtonsoft.Json;
 using Taller2Divisas.Models;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace Taller2Divisas.ViewModels
@@ -17,7 +19,10 @@ namespace Taller2Divisas.ViewModels
         private bool isRunning;
         private bool isEnabled;
         private ExchangeRates exchangeRates;
+        private RateNames rateNames;
         private string message;
+        private double sourceRate;
+        private double targetRate;
         #endregion
 
 
@@ -72,14 +77,28 @@ namespace Taller2Divisas.ViewModels
 
         public double SourceRate
         {
-            get;
-            set;
+            get{
+                return sourceRate;
+            }
+            set{
+                if(sourceRate != value){
+                    sourceRate = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SourceRate"));
+                }
+            }
         }
 
         public double TargetRate
         {
-            get;
-            set;
+            get{
+                return targetRate;
+            }
+            set{
+                if(targetRate != value){
+                    targetRate = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TargetRate"));
+                }
+            }
         }
 
         public string Message
@@ -114,17 +133,45 @@ namespace Taller2Divisas.ViewModels
 
         private void LoadRates()
         {
-            Rates.Clear();
+
+            var rateValues = new List<RateValue>();
             var type = typeof(Rates);
             var properties = type.GetRuntimeFields();
 
-            foreach (var property in properties)
-            {
+            foreach(var property in properties){
                 var code = property.Name.Substring(1, 3);
-                Rates.Add(new Rate
+                rateValues.Add(new RateValue
                 {
                     Code = code,
-                    TaxRate = (double)property.GetValue(exchangeRates.Rates),
+                    TaxRate = (double)property.GetValue(exchangeRates.Rates)
+                });
+            };
+
+            //names
+            var rateNamesList = new List<RateName>();
+            type = typeof(RateNames);
+            properties = type.GetRuntimeFields();
+
+            foreach(var property in properties){
+                var code = property.Name.Substring(1, 3);
+                rateNamesList.Add(new RateName
+                {
+                    Code = code,
+                    Name = (string)property.GetValue(rateNames)
+                });
+            }
+
+            Rates.Clear();
+
+            List<Rate> rateList = joinList(rateValues, rateNamesList);
+
+            foreach (Rate property in rateList)
+            {
+                Rates.Add(new Rate
+                {
+                    Code = property.Code,
+                    TaxRate = (double)property.TaxRate,
+                    Name = property.FullName
                 });
             }
         }
@@ -135,19 +182,29 @@ namespace Taller2Divisas.ViewModels
             try
             {
                 var client = new HttpClient();
+                var client2 = new HttpClient();
+
                 client.BaseAddress = new Uri("https://openexchangerates.org");
                 var url = "/api/latest.json?app_id=f490efbcd52d48ee98fd62cf33c47b9e";
-                var response = await client.GetAsync(url);
-                if (!response.IsSuccessStatusCode)
+                var responseRates = await client.GetAsync(url);
+
+                client2.BaseAddress = new Uri("https://gist.githubusercontent.com");
+                var urlNames = "/picodotdev/88512f73b61bc11a2da4/raw/9407514be22a2f1d569e75d6b5a58bd5f0ebbad8";
+                var responseNames = await client2.GetAsync(urlNames);
+
+                if (!responseRates.IsSuccessStatusCode || !responseNames.IsSuccessStatusCode)
                 {
-                    await App.Current.MainPage.DisplayAlert("Error", response.StatusCode.ToString(), "Aceptar");
+                    await App.Current.MainPage.DisplayAlert("Error", responseRates.StatusCode.ToString(), "Aceptar");
                     IsRunning = false;
                     IsEnabled = false;
                     return;
                 }
 
-                var result = await response.Content.ReadAsStringAsync();
-                exchangeRates = JsonConvert.DeserializeObject<ExchangeRates>(result);
+                var result1 = await responseRates.Content.ReadAsStringAsync();
+                exchangeRates = JsonConvert.DeserializeObject<ExchangeRates>(result1);
+
+                var result2 = await responseNames.Content.ReadAsStringAsync();
+                rateNames = JsonConvert.DeserializeObject<RateNames>(result2);
             }
             catch (Exception ex)
             {
@@ -172,9 +229,74 @@ namespace Taller2Divisas.ViewModels
             }
         }
 
-        private void ConvertMoney()
+	    private async void ConvertMoney()
+			{
+				if (Amount <= 0)
+				{
+					await App.Current.MainPage.DisplayAlert(
+						"Error",
+						"Debes ingresar un valor a convertir",
+						"Aceptar");
+					return;
+				}
+
+				if (SourceRate == 0)
+				{
+					await App.Current.MainPage.DisplayAlert(
+						"Error",
+						"Debes seleccionar la moneda origen",
+						"Aceptar");
+					return;
+				}
+
+				if (TargetRate == 0)
+				{
+					await App.Current.MainPage.DisplayAlert(
+						"Error",
+						"Debes seleccionar la moneda destino",
+						"Aceptar");
+					return;
+				}
+
+				decimal amountConverted = Amount / (decimal)SourceRate * (decimal)TargetRate;
+
+				Message = string.Format("{0:N2} = {1:N2}", Amount, amountConverted);
+			}
+
+        public ICommand ChangeCommand
         {
-            throw new NotImplementedException();
+            get{
+                return new RelayCommand(Change);
+            }
+        }
+
+        private void Change()
+        {
+            var aux = SourceRate;
+            SourceRate = TargetRate;
+            TargetRate = aux;
+        }
+
+        private List<Rate> joinList(List<RateValue> l1, List<RateName> l2){
+
+            var rateList = new List<Rate>();
+
+            for (var i = 0; i < l1.Count(); i++){
+                var auxCodeValue = l1[i].Code;
+                for (var j = 0; j < l2.Count(); j++){
+                    if(auxCodeValue == l2[j].Code){
+                        rateList.Add(new Rate
+                        {
+                            Code = auxCodeValue,
+                            TaxRate = (double)l1[i].TaxRate,
+                            Name = l2[j].Name
+                        });
+                        break;
+                    }
+                }
+            }
+
+            return rateList;
         }
 
         #endregion
